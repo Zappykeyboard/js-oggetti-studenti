e44e423df3d28ac79b37830193cbbf6319bcfae5
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-10 16:56:50 +0200

    aggiunte sezioni 1 e 2

---
+++ app.js
@@ -0,0 +1,36 @@
+//Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+/* var student = {
+  "nome": "Edoardo",
+  "cognome": "D'Ambrosio",
+  "età": 29
+};
+
+for (key in student){
+  console.log(student[key]);
+}
+
+ */
+
+
+//- Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+/* var studenti = [
+  {
+    "nome": "Edoardo",
+    "cognome": "D'Ambrosio"
+  },
+  {
+    "nome": "Marco",
+    "cognome": "Rossi" 
+  },
+  {
+    "nome": "Giovanni",
+    "cognome": "Verdi" 
+  }
+];
+
+for (var i=0; i < studenti.length; i++){
+  for (key in studenti[i]){
+    console.log(studenti[i][key]);
+  }
+} */
+
